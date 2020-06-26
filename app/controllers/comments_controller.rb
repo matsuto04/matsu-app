86ea5db301143c84e27ea331574ff44eae706cd3
matsuto04 <toshi.drdr@gmail.com>
2020-06-26 12:20:21 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :set_params
+
   def index
+    @comments =@group.comments.includes(:user)
+    @comment =Comment.new
+  end
+
+  def create
+    @comment = @group.comments.new(comment_params)
+    if @comment.save
+      redirect_to group_comments_path(@group)
+    else
+      @comments = @group.comments.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_params
+    @group = Group.find(params[:group_id])
   end
 end
